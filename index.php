85f052ed13ec3fbfdf2d7c0c200e700651d7bb92
manonBr <manon.bruimaud@gmail.com>
2023-10-06 13:21:27 +0200

    :style: Update portfolio UI

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             <h3 class="section__title">Présentation</h3>
         </div>
         <div class="presentation__description">
-            <p>Le développement et les technologies me <span class="emphasized">passionnent</span>, les défis que comportent chaque nouveau projet me stimule. Autodidacte spécialisée dans le <span class="emphasized">front-end</span>, je maitrise également tout à fait les fondements du développement back-end : PHP, Laravel, POO, etc. À chaque projet, je veille à construire un code optimisé tout en veillant à produire un site répondant aux règles élémentaires d'<span class="emphasized">accessibilités</span>.J'ai pour habitude de travailler avec un outil de versionning tel que <span class="emphasized">GIT</span> ainsi qu'un préprocesseur CSS comme <span class="emphasized">SASS</span>.</p>
+            <p>Le développement et les technologies me <span class="emphasized">passionnent</span>, les défis que comportent chaque nouveau projet me stimulent. Autodidacte spécialisée dans le <span class="emphasized">front-end</span>, je maitrise également tout à fait les fondements du développement back-end : PHP, Laravel, POO, etc. À chaque projet, je veille à construire un code optimisé tout en veillant à produire un site répondant aux règles élémentaires d'<span class="emphasized">accessibilités</span>.J'ai pour habitude de travailler avec un outil de versionning tel que <span class="emphasized">GIT</span> ainsi qu'un préprocesseur CSS comme <span class="emphasized">SASS</span>.</p>
         </div>
     </section>
 </div>
@@ -38,47 +38,30 @@
     <div class="container">
         <h3 class="section__title">Portfolio</h3>
         <div>
-            <!-- <div class="portfolio__filter">
-                <button class="btn btn--secondary current" data-filter="all">Tout</button>
-                <button class="btn btn--secondary" data-filter="pro">Pros (en agence)</button>
-                <button class="btn btn--secondary" data-filter="perso">Persos</button>
-            </div> -->
             <div class="portfolio__wrapper">
-                <a href="https://www.journaldelagence.com" target="_blank" class="portfolio__item" data-type="pro">
-                    <span class="portfolio__link">
-                        <img src="/assets/img/portfolio/jda.png" alt="Projet Journal de l'agence">
-                    </span>
-                    <span class="portfolio__item__subttl">Techno &bull; Techno</span>
-                </a>
-                <div class="portfolio__item portfolio__item--divider"></div>
-                <a href="https://www.hectoria.com" target="_blank" class="portfolio__item" data-type="pro">
-                    <span class="portfolio__link">
-                        <img src="/assets/img/portfolio/hectoria.png" alt="Projet Hectoria">
-                    </span>
-                    <span class="portfolio__item__subttl">Techno &bull; Techno &bull; Techno</span>
-                </a>
-                <div class="portfolio__item portfolio__item--divider"></div>
-                <a href="https://www.donbastineo.fr" target="_blank" class="portfolio__item" data-type="pro">
-                    <span class="portfolio__link">
-                        <img src="/assets/img/portfolio/donBastineo.png" alt="Projet Don Bastineo">
-                    </span>
-                    <span class="portfolio__item__subttl">Techno &bull; Techno &bull; Techno</span>
-                </a>
-                <div class="portfolio__item portfolio__item--divider"></div>
-                <a href="/" target="_blank" class="portfolio__item" data-type="perso">
-                    <span class="portfolio__link">
-                        <img src="/assets/img/portfolio/proventil33.png" alt="Projet Proventil33">
-                    </span>
-                    <span class="portfolio__item__subttl">Techno &bull; Techno</span>
-                </a>
-                <div class="portfolio__item portfolio__item--divider"></div>
-                <a href="/" target="_blank" class="portfolio__item" data-type="perso">
-                    <span class="portfolio__link">
-                        <img src="/assets/img/portfolio/proventil33.png" alt="Projet CupcakeAgency">
-                    </span>
-                    <span class="portfolio__item__subttl">Techno &bull; Techno &bull; Techno</span>
-                </a>
-                <div class="portfolio__item portfolio__item--divider"></div>
+                <div class="portfolio__item">
+                    <a href="https://www.manonbgabrielle.fr" target="_blank" class="portfolio__link">
+                        <img src="/assets/img/portfolio/manonbgabrielle.jpg" alt="Mon site internet d'auteure">
+                        <div class="portfolio__item__description">
+                            <p class="portfolio__item__name">manonbgabrielle.fr</p>
+                            <p class="portfolio__item__content">Projet personnelle. Création d'un site internet pour mon activité d'auteure non-publiée.</p>
+                            <p class="portfolio__item__technos">ReactJs &bull; NodeJS &bull; MongoDB</p>
+                        </div>
+                    </a>
+                    <a class="btn btn--primary" href="https://github.com/manonBr/manonautrice" target="_blank">GitHub</a>
+                    <a class="btn btn--secondary" href="https://www.manonbgabrielle.fr" target="_blank">Site internet</a>
+                </div>
+                <div class="portfolio__item">
+                    <a href="https://www.journaldelagence.com" target="_blank" class="portfolio__link">
+                        <img src="/assets/img/portfolio/jda.jpg" alt="Projet Journal de l'agence">
+                        <div class="portfolio__item__description">
+                            <p class="portfolio__item__name">journaldelagence.fr</p>
+                            <p class="portfolio__item__content">Site internet réalisé en agence. J'ai été en charge de l'intégration de la maquette et de la création intégrale du template.</p>
+                            <p class="portfolio__item__technos">Wordpress &bull; PHP &bull; Sass</p>
+                        </div>
+                    </a>
+                    <a class="btn btn--secondary" href="https://www.journaldelagence.com" target="_blank">Site internet</a>
+                </div>
             </div>
         </div>
     </div>
@@ -93,30 +76,6 @@
                 <span class="technos__rollover">Javascript</span>
             </div>
             <div class="technos__item">
-                <img src="/assets/img/technos/git-withoutborder.png" alt="GIT">
-                <span class="technos__rollover">GIT</span>
-            </div>
-            <div class="technos__item">
-                <img src="/assets/img/technos/laravel-withoutborder.png" alt="Laravel">
-                <span class="technos__rollover">Laravel</span>
-            </div>
-            <div class="technos__item">
-                <img src="/assets/img/technos/mysql-withoutborder.png" alt="MySQL">
-                <span class="technos__rollover">MySQL</span>
-            </div>
-            <div class="technos__item">
-                <img src="/assets/img/technos/npm-withoutborder.png" alt="NPM">
-                <span class="technos__rollover">NPM</span>
-            </div>
-            <div class="technos__item">
-                <img src="/assets/img/technos/php-withoutborder.png" alt="PHP">
-                <span class="technos__rollover">PHP</span>
-            </div>
-            <div class="technos__item">
-                <img src="/assets/img/technos/prestashop-withoutborder.png" alt="Prestashop">
-                <span class="technos__rollover">Prestashop</span>
-            </div>
-            <div class="technos__item">
                 <img src="/assets/img/technos/react-withoutborder.png" alt="React">
                 <span class="technos__rollover">React</span>
             </div>
@@ -125,12 +84,36 @@
                 <span class="technos__rollover">Sass</span>
             </div>
             <div class="technos__item">
-                <img src="/assets/img/technos/webpack-withoutborder.png" alt="Webpack">
-                <span class="technos__rollover">Webpack</span>
+                <img src="/assets/img/technos/axios-withoutborder.png" alt="Axios">
+                <span class="technos__rollover">Axios</span>
             </div>
             <div class="technos__item">
-                <img src="/assets/img/technos/wordpress-withoutborder.png" alt="Wordpress">
-                <span class="technos__rollover">Wordpress</span>
+                <img src="/assets/img/technos/git-withoutborder.png" alt="GIT">
+                <span class="technos__rollover">GIT</span>
+            </div>
+            <div class="technos__item">
+                <img src="/assets/img/technos/nodejs-withoutborder.png" alt="NodeJS">
+                <span class="technos__rollover">NodeJS</span>
+            </div>
+            <div class="technos__item">
+                <img src="/assets/img/technos/express-withoutborder.png" alt="Express">
+                <span class="technos__rollover">Express</span>
+            </div>
+            <div class="technos__item">
+                <img src="/assets/img/technos/mongodb-withoutborder.png" alt="MongoDB">
+                <span class="technos__rollover">MongoDB</span>
+            </div>
+            <div class="technos__item">
+                <img src="/assets/img/technos/php-withoutborder.png" alt="PHP">
+                <span class="technos__rollover">PHP</span>
+            </div>
+            <div class="technos__item">
+                <img src="/assets/img/technos/mysql-withoutborder.png" alt="MySQL">
+                <span class="technos__rollover">MySQL</span>
+            </div>
+            <div class="technos__item">
+                <img src="/assets/img/technos/laravel-withoutborder.png" alt="Laravel">
+                <span class="technos__rollover">Laravel</span>
             </div>
         </div>
         <div class="technos__wrapper">
